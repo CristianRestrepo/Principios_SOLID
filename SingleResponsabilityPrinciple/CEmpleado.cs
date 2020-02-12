3591d713ad2f6f98303a4680ce5a16c4c38937a1
Cristian Camilo Restrepo Reyes <Cris940604@hotmail.com>
2020-02-12 14:36:45 -0500

    Creación Proyecto Principios SOLID

---
+++ SingleResponsabilityPrinciple/CEmpleado.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsability
+{
+    class CEmpleado
+    {
+
+        private string nombre;
+        private string puesto;
+        private int edad;
+        private double sueldo;
+
+        public CEmpleado(string pNombre, string pPuesto, int pEdad, double pSueldo)
+        {
+            nombre = pNombre;
+            puesto = pPuesto;
+            edad = pEdad;
+            sueldo = pSueldo;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}, {2}, {3}", nombre, puesto, edad, sueldo);
+        }
+
+
+
+        //Métodos que no son responsabilidad del empleado
+        public double CalcularImpuesto() {
+            return sueldo * 0.35d;
+        }
+
+        public void PagarImpuesto() {
+            double imp = CalcularImpuesto();
+            Console.WriteLine("Se pago {0} en impuesto por parte de {1}", imp, nombre);
+           
+        }
+
+    }
+}
---
+++ SingleResponsabilityPrinciple/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CEmpleado empleado = new CEmpleado("Cristian", "Programador", 24, 3000000);
+            Console.WriteLine(empleado);
+            empleado.PagarImpuesto();
+            Console.ReadLine();
+        }
+    }
+}
